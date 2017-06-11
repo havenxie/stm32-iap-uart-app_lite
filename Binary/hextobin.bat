cb92ff56a0c804f47d9cef817df77ba03f55a047
havenxie <1428876581@qq.com>
2017-06-11 23:39:30 +0800

    add readme

---
+++ Binary/hextobin.bat
@@ -0,0 +1,7 @@
+if exist .\..\*.hex (.\..\Binary\hex2bin.exe  .\..\OUTPUT\*.hex)
+if exist .\..\*.hex (.\..\Binary\hex2bin.exe  .\..\OUTPUT\*.hex)
+if exist .\..\*.hex (.\..\Binary\hex2bin.exe  .\..\OUTPUT\*.hex)
+if exist .\..\*.hex (.\..\Binary\hex2bin.exe  .\..\OUTPUT\*.hex)
+if exist .\..\*.hex (.\..\Binary\hex2bin.exe  .\..\OUTPUT\*.hex)
+if exist .\..\*.hex (.\..\Binary\hex2bin.exe  .\..\OUTPUT\*.hex)
+exit
