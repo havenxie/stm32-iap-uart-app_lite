84a588323748aeeeeec2ce720c25383e10b77e37
havenxie <1428876581@qq.com>
2017-07-02 18:06:35 +0800

    test on stm32f103c8

--- HARDWARE/LED/led.h
+++ HARDWARE/LED/led.h
@@ -1,14 +1,9 @@
 #ifndef __LED_H
 #define __LED_H	 
 #include "stm32f10x.h"
-//Mini STM32开发板
-//LED驱动代码			 
-//正点原子@ALIENTEK
-//2012/2/27
 
 //LED端口定义
-#define LED0 PAout(8)// PA8
-#define LED1 PDout(2)// PD2	
+#define LED0 PCout(13)// PA8
 
 void LED_Init(void);//初始化
 
