799ab59522481d555bd1960ebf36fcc56617fd2f
havenxie <1428876581@qq.com>
2017-07-03 23:45:19 +0800

    add transmater

--- HARDWARE/KEY/key.c
+++ HARDWARE/KEY/key.c
@@ -3,16 +3,6 @@
 #include "sys.h" 
 #include "delay.h"
 
-//////////////////////////////////////////////////////////////////////////////////	 
-//本程序只供学习使用，未经作者许可，不得用于其它任何用途
-//Mini STM32开发板
-//按键输入 驱动代码		   
-//技术论坛:www.openedv.com
-//修改日期:2011/11/28 
-//版本：V1.1
-//版权所有，盗版必究。
-//Copyright(C) ALIENTEK  2009-2019
-//All rights reserved
 //********************************************************************************
  
 void KEY_Init(void) //IO初始化
@@ -43,19 +33,16 @@ u8 KEY_Scan(void)
 		key_up=0;
 		if(KEY0==0)
 		{
-	 
-			 GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable, ENABLE);
+			GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable, ENABLE);
 			return 1;
 		}
 		else if(KEY1==0)
 		{
-	 	 
 	     	GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable, ENABLE);
 			return 2;
 		}
 		else if(KEY2==1)
 		{
-	 
 	    	GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable, ENABLE);
 			return 3;
 		}
--- USER/transmitter.c
+++ USER/transmitter.c
@@ -1,33 +1,106 @@
 #include <string.h>
-#include "led.h"
+#include "key.h"
 #include "timer.h"
 #include "delay.h"
 #include "sys.h"
 #include "usart.h"
 #include "iap.h"
-//A6 A7 B0 B1
 
 //#define USE_IAP
-#define CH1 1
-#define CH2 2
-#define CH3 3
-#define CH4 4
+static u8 current_ch = 0;
+static u16 current_val = 0;
+static u16 ch1_val, ch2_val, ch3_val, ch4_val = 0;
 
+#define CH1 1            //PA6
+#define CH2 2            //PA7
+#define CH3 3            //PB0
+#define CH4 4            //PB1
+
+#define FUN_KEY 1        //PA13
+#define VAL_ADD_KEY 2    //PA15
+#define VAL_SUB_KEY 3    //PA0
+
+#define CHx_Change_Val(rule, value) \
+{\
+	if(rule == VAL_ADD_KEY)\
+		value = value >= 700 ? 700 : value + 10;\
+	else if(rule == VAL_SUB_KEY)\
+		value = value <= 10 ? 0 : value - 10;\
+	current_val = value;\
+}
+
+void Calc_CHx_Val(u8 rule)
+{
+	switch(current_ch)
+	{
+		case CH1:
+			CHx_Change_Val(rule, ch1_val);
+			break;
+		case CH2:
+			CHx_Change_Val(rule, ch2_val);
+			break;
+		case CH3:
+			CHx_Change_Val(rule, ch3_val);
+			break;
+		case CH4:
+			CHx_Change_Val(rule, ch4_val);
+			break;
+		default: 
+			break;
+	}
+}
+
+void Send_Meg(u8 rule)
+{
+	if(rule == FUN_KEY)
+	{
+		USART_RX_BUF[0] = '@';
+		USART_RX_BUF[1] = current_ch + '0';
+		USART_RX_BUF[2] = '\r';
+		USART_RX_BUF[3] = '\n';
+		printf("%s", (char *)USART_RX_BUF);
+		memset(USART_RX_BUF, 0, 4);
+	}
+	else
+	{
+		USART_RX_BUF[0] = '$';
+		USART_RX_BUF[1] = current_val / 100 + '0';
+		USART_RX_BUF[2] = current_val % 100 / 10 + '0';
+		USART_RX_BUF[3] = current_val % 10 + '0';
+		USART_RX_BUF[4] = '\r';
+		USART_RX_BUF[5] = '\n';
+		printf("%s", (char *)USART_RX_BUF);	
+		memset(USART_RX_BUF, 0, 6);
+	}
+
+}
 
 int main(void)
 {
 	IAP_Init();          //程序进入之后首先执行iap初始化 将标志位清除
 	delay_init(72);	     //延时初始化
 	NVIC_Configuration();//设置NVIC中断分组2:2位抢占优先级，2位响应优先级
-	uart_init(115200);   //串口初始化为9600
- 	LED_Init();	         //LED端口初始化
-	TIM3_PWM_Init(719, 0);//不分频。PWM频率=72000/719=10Khz
+	uart_init(115200);   //串口初始化为115200
+ 	KEY_Init();	         //LED端口初始化
 	while(1)
 	{
-		if(USART_RX_STA&0x80)
-		{					 
-			USART_RX_STA=0;
-			IAP_Handle(USART_RX_BUF);
+		switch(KEY_Scan())
+		{
+			case FUN_KEY:
+				current_ch = (current_ch + 1) % 5;
+				current_ch = current_ch == 0 ? 1 : current_ch;
+				Send_Meg(FUN_KEY);
+				break;
+			case VAL_ADD_KEY:
+				Calc_CHx_Val(VAL_ADD_KEY);
+				Send_Meg(VAL_ADD_KEY);
+				break;
+			case VAL_SUB_KEY:
+				Calc_CHx_Val(VAL_SUB_KEY);
+				Send_Meg(VAL_SUB_KEY);
+				break;
+			default: 
+				break;
 		}
 	}	 
 }
