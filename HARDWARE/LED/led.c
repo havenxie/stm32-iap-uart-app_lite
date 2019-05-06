bc628cf3399f19185c0ed608b8e37b4efd996134
havenxie <1428876581@qq.com>
2019-05-06 22:33:44 +0800

    local building

--- HARDWARE/LED/led.c
+++ HARDWARE/LED/led.c
@@ -8,7 +8,6 @@ void LED_Init(void)
  GPIO_InitTypeDef  GPIO_InitStructure;
  	
  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);	 //使能PC端口时钟
-	
  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;				 //LED0-->PC13 端口配置
  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 		 //推挽输出
  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
