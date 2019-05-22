5344dea4e1b9cbd3afc7b4b2ee08f70a3d4cb9b2
havenxie <1428876581@qq.com>
2019-05-22 23:05:08 +0800

    工程不支持DAP-Link的SWO,需要到X-LINK项目下寻找工程

--- SYSTEM/usart/usart.c
+++ SYSTEM/usart/usart.c
@@ -16,7 +16,7 @@ int fputc(int ch, FILE *f)
 {      
 	if (DEMCR & TRCENA)  
     {  
-        while (ITM_Port32(0) == 0);  
+        while (ITM_Port32(0) == 0){};
   
         ITM_Port8(0) = ch;  
     }  
